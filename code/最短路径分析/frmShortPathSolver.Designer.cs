1a47258558548b96038c7b81ffeefa4faec2d6ec
Hao Jiang <jianghao0425@qq.com>
2020-01-10 16:44:13 +0800

    null

---
+++ code/最短路径分析/frmShortPathSolver.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace 最短路径分析
+{
+    partial class frmShortPathSolver
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmShortPathSolver));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.文件ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.open = new System.Windows.Forms.ToolStripMenuItem();
+            this.最短路径分析ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addStops = new System.Windows.Forms.ToolStripMenuItem();
+            this.addBarriers = new System.Windows.Forms.ToolStripMenuItem();
+            this.routeSolver = new System.Windows.Forms.ToolStripMenuItem();
+            this.clear = new System.Windows.Forms.ToolStripMenuItem();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.axLicenseControl1 = new ESRI.ArcGIS.Controls.AxLicenseControl();
+            this.axTOCControl1 = new ESRI.ArcGIS.Controls.AxTOCControl();
+            this.mainMapControl = new ESRI.ArcGIS.Controls.AxMapControl();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.axLicenseControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axTOCControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mainMapControl)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.文件ToolStripMenuItem,
+            this.最短路径分析ToolStripMenuItem,
+            this.clear});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(8, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(817, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 文件ToolStripMenuItem
+            // 
+            this.文件ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.open});
+            this.文件ToolStripMenuItem.Name = "文件ToolStripMenuItem";
+            this.文件ToolStripMenuItem.Size = new System.Drawing.Size(51, 24);
+            this.文件ToolStripMenuItem.Text = "文件";
+            // 
+            // open
+            // 
+            this.open.Name = "open";
+            this.open.Size = new System.Drawing.Size(148, 24);
+            this.open.Text = "打开&Open";
+            this.open.Click += new System.EventHandler(this.open_Click);
+            // 
+            // 最短路径分析ToolStripMenuItem
+            // 
+            this.最短路径分析ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addStops,
+            this.addBarriers,
+            this.routeSolver});
+            this.最短路径分析ToolStripMenuItem.Name = "最短路径分析ToolStripMenuItem";
+            this.最短路径分析ToolStripMenuItem.Size = new System.Drawing.Size(111, 24);
+            this.最短路径分析ToolStripMenuItem.Text = "最短路径分析";
+            // 
+            // addStops
+            // 
+            this.addStops.Image = ((System.Drawing.Image)(resources.GetObject("addStops.Image")));
+            this.addStops.Name = "addStops";
+            this.addStops.Size = new System.Drawing.Size(138, 24);
+            this.addStops.Text = "添加站点";
+            this.addStops.Click += new System.EventHandler(this.addStops_Click);
+            // 
+            // addBarriers
+            // 
+            this.addBarriers.Image = ((System.Drawing.Image)(resources.GetObject("addBarriers.Image")));
+            this.addBarriers.Name = "addBarriers";
+            this.addBarriers.Size = new System.Drawing.Size(138, 24);
+            this.addBarriers.Text = "添加障碍";
+            this.addBarriers.Click += new System.EventHandler(this.addBarriers_Click);
+            // 
+            // routeSolver
+            // 
+            this.routeSolver.Image = ((System.Drawing.Image)(resources.GetObject("routeSolver.Image")));
+            this.routeSolver.Name = "routeSolver";
+            this.routeSolver.Size = new System.Drawing.Size(138, 24);
+            this.routeSolver.Text = "路径解决";
+            this.routeSolver.Click += new System.EventHandler(this.routeSolver_Click);
+            // 
+            // clear
+            // 
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(81, 24);
+            this.clear.Text = "清除分析";
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 28);
+            this.splitContainer1.Margin = new System.Windows.Forms.Padding(4);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.splitContainer1.Panel1.Controls.Add(this.axTOCControl1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.splitContainer1.Panel2.Controls.Add(this.mainMapControl);
+            this.splitContainer1.Size = new System.Drawing.Size(817, 477);
+            this.splitContainer1.SplitterDistance = 271;
+            this.splitContainer1.SplitterWidth = 5;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // axLicenseControl1
+            // 
+            this.axLicenseControl1.Enabled = true;
+            this.axLicenseControl1.Location = new System.Drawing.Point(767, -2);
+            this.axLicenseControl1.Name = "axLicenseControl1";
+            this.axLicenseControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axLicenseControl1.OcxState")));
+            this.axLicenseControl1.Size = new System.Drawing.Size(32, 32);
+            this.axLicenseControl1.TabIndex = 3;
+            // 
+            // axTOCControl1
+            // 
+            this.axTOCControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.axTOCControl1.Location = new System.Drawing.Point(0, 0);
+            this.axTOCControl1.Margin = new System.Windows.Forms.Padding(4);
+            this.axTOCControl1.Name = "axTOCControl1";
+            this.axTOCControl1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axTOCControl1.OcxState")));
+            this.axTOCControl1.Size = new System.Drawing.Size(271, 477);
+            this.axTOCControl1.TabIndex = 0;
+            // 
+            // mainMapControl
+            // 
+            this.mainMapControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainMapControl.Location = new System.Drawing.Point(0, 0);
+            this.mainMapControl.Margin = new System.Windows.Forms.Padding(4);
+            this.mainMapControl.Name = "mainMapControl";
+            this.mainMapControl.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("mainMapControl.OcxState")));
+            this.mainMapControl.Size = new System.Drawing.Size(541, 477);
+            this.mainMapControl.TabIndex = 0;
+            this.mainMapControl.OnMouseDown += new ESRI.ArcGIS.Controls.IMapControlEvents2_Ax_OnMouseDownEventHandler(this.axMapControl1_OnMouseDown);
+            // 
+            // frmShortPathSolver
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(817, 505);
+            this.Controls.Add(this.axLicenseControl1);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "frmShortPathSolver";
+            this.Text = "最短路径分析";
+            this.Load += new System.EventHandler(this.frmShortPathSolver_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.axLicenseControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axTOCControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mainMapControl)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 文件ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem open;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private ESRI.ArcGIS.Controls.AxTOCControl axTOCControl1;
+        private ESRI.ArcGIS.Controls.AxMapControl mainMapControl;
+        private System.Windows.Forms.ToolStripMenuItem 最短路径分析ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addStops;
+        private System.Windows.Forms.ToolStripMenuItem addBarriers;
+        private System.Windows.Forms.ToolStripMenuItem routeSolver;
+        private System.Windows.Forms.ToolStripMenuItem clear;
+        private ESRI.ArcGIS.Controls.AxLicenseControl axLicenseControl1;
+    }
+}
+
---
+++ code/最短路径分析/frmShortPathSolver.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ESRI.ArcGIS.Controls;
+using ESRI.ArcGIS.Carto;
+using ESRI.ArcGIS.SystemUI;
+using ESRI.ArcGIS.Geodatabase;
+
+namespace 最短路径分析
+{
+    public partial class frmShortPathSolver : Form
+    {
+        IMapDocument mapDocument;
+        public IFeatureWorkspace pFWorkspace;
+
+        string path = System.AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
+        public frmShortPathSolver()
+        {
+            InitializeComponent();
+            axTOCControl1.SetBuddyControl(mainMapControl);
+        }
+
+        private void open_Click(object sender, EventArgs e)
+        {
+            mapDocument = new MapDocumentClass();
+            try
+            {
+                System.Windows.Forms.OpenFileDialog openFileDialog;
+                openFileDialog = new OpenFileDialog();
+                openFileDialog.Title = "打开图层文件";
+                openFileDialog.Filter = "map documents(*.mxd)|*.mxd";
+                openFileDialog.ShowDialog();
+                string filePath = openFileDialog.FileName;
+                mapDocument.Open(filePath, "");
+                for (int i = 0; i < mapDocument.MapCount; i++)
+                {
+                    mainMapControl.Map = mapDocument.get_Map(i);
+                }
+                mainMapControl.Refresh();
+                 
+
+              
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("加载失败" + ex.ToString());
+            }
+        }
+        //加载站点
+        private void addStops_Click(object sender, EventArgs e)
+        {
+            ICommand pCommand;
+            pCommand = new AddNetStopsTool();
+            pCommand.OnCreate(mainMapControl.Object);
+            mainMapControl.CurrentTool = pCommand as ITool;
+            pCommand = null;
+        }
+        //加载障碍点
+        private void addBarriers_Click(object sender, EventArgs e)
+        {
+            ICommand pCommand;
+            pCommand = new AddNetBarriesTool();
+            pCommand.OnCreate(mainMapControl.Object);
+            mainMapControl.CurrentTool = pCommand as ITool;
+            pCommand = null;
+        }
+        //最短路径分析
+        private void routeSolver_Click(object sender, EventArgs e)
+        {
+            ICommand pCommand;
+            pCommand = new ShortPathSolveCommand();
+            pCommand.OnCreate(mainMapControl.Object);
+            pCommand.OnClick();
+            pCommand = null;
+        }
+        //清除分析
+        private void clear_Click(object sender, EventArgs e)
+        {
+            mainMapControl.CurrentTool = null;
+            try
+            {
+                string name = NetWorkAnalysClass.getPath(path) + "\\HuanbaoGeodatabase.gdb";
+                //打开工作空间
+                pFWorkspace = NetWorkAnalysClass.OpenWorkspace(name) as IFeatureWorkspace;
+                IGraphicsContainer pGrap = this.mainMapControl.ActiveView as IGraphicsContainer;
+                pGrap.DeleteAllElements();//删除所添加的图片要素
+                IFeatureClass inputFClass = pFWorkspace.OpenFeatureClass("Stops");
+                //删除站点要素
+                if (inputFClass.FeatureCount(null) > 0)
+                {
+                    ITable pTable = inputFClass as ITable;
+                    pTable.DeleteSearchedRows(null);                 
+                }
+                IFeatureClass barriesFClass = pFWorkspace.OpenFeatureClass("Barries");//删除障碍点要素
+                if (barriesFClass.FeatureCount(null) > 0)
+                {
+                    ITable pTable = barriesFClass as ITable;
+                    pTable.DeleteSearchedRows(null);
+                }
+                for (int i = 0; i < mainMapControl.LayerCount; i++)//删除分析结果
+                {
+                    ILayer pLayer = mainMapControl.get_Layer(i);
+                    if (pLayer.Name == ShortPathSolveCommand.m_NAContext.Solver.DisplayName)
+                    {
+                        mainMapControl.DeleteLayer(i);
+                        break;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            this.mainMapControl.Refresh();
+        }
+
+        private void frmShortPathSolver_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void axMapControl1_OnMouseDown(object sender, ESRI.ArcGIS.Controls.IMapControlEvents2_OnMouseDownEvent e)
+        {
+
+        }
+    }
+}
